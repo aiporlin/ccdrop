f885690dfc4d5319c9ee0ad01e22629bd11921fd
aiporlin <aiporlin@hotmail.com>
2025-10-20 15:54:35 +0800

    mee2

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,32 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": [
+      "static/chunks/e60ef129113f6e24.js",
+      "static/chunks/a69386890439c854.js",
+      "static/chunks/turbopack-136052ddac34579d.js"
+    ],
+    "/_error": [
+      "static/chunks/17722e3ac4e00587.js",
+      "static/chunks/a69386890439c854.js",
+      "static/chunks/turbopack-b564b1cf04be9e97.js"
+    ]
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/cdce9f70eed45fe1.js",
+    "static/chunks/34c4658583b69a91.js",
+    "static/chunks/3982aca350007783.js",
+    "static/chunks/turbopack-92865461c21ed104.js"
+  ],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
